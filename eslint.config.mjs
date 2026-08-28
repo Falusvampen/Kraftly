a0f2b1059a5b22dd5bf8a6ff913ae90947603e80
Fabian144 <fabianyousif24@gmail.com>
2026-08-28 14:12:41 +0200

    feat: install Cypress, add smoke test and e2e test for invoices, add decision motivation for choice of Cypress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import pluginCypress from 'eslint-plugin-cypress';
 import pluginVue from 'eslint-plugin-vue';
 import { defineConfig } from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
   { ignores: ['dist/**', 'node_modules/**'] },
+
   {
     files: ['**/*.{js,mjs,cjs,vue}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
     rules: {
       'no-console': 'warn',
       'no-alert': 'warn',
@@ -19,6 +23,12 @@ export default defineConfig([
       'vue/multi-word-component-names': 'off',
     },
   },
+
+  {
+    files: ['cypress/**/*.{js,ts,jsx,tsx}'],
+    extends: [pluginCypress.configs.recommended],
+  },
+
   pluginVue.configs['flat/essential'],
   eslintConfigPrettier,
 ]);
