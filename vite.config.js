31b0a20d3a4b924b4772027203701a33568a38f1
Fabian144 <fabianyousif24@gmail.com>
2026-08-30 11:53:32 +0200

    feat: install jsdom to test vue components with vitest (#36)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // config from a starter template, seems to work /M
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'jsdom',
+  },
 });
